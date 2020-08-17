be87a67263b587b372cd86d97f47ea95ebe3e5a5
AayushKucheria <aayush.kucheria@gmail.com>
2020-08-18 00:32:13 +0300

    Finished integrating khoa's model

--- app/ai.py
+++ app/ai.py
@@ -1,30 +1,45 @@
-# from tslearn.clustering import TimeSeriesKMeans
+from tslearn.clustering import TimeSeriesKMeans
 import numpy as np
 import crud
 from sqlalchemy.orm import Session
 from database import SessionLocal
+from sklearn.metrics import confusion_matrix
+import math
 
 
 def get_session_data(p_id: int):
     db: Session = SessionLocal()
     latest_session = crud.get_latest_session_table_by_id(db, p_id)
-    data = crud.get_table_data(db, latest_session)
-    print(type(data))
-    (print(data[0]))
+    initial_data = crud.get_table_data(db, latest_session)
+    test = []
+    for row in initial_data:
+        temp = []
+        for elem in row[1:]:
+            temp.append([int(elem)])
+        test.append(np.asarray(temp))
 
-
+    a = np.array(test)
+    return a
 
 def knn(p_id):
-    test: list[np.ndarray[np.ndarray[np.int]]]
-    get_session_data(p_id)
-    # model = TimeSeriesKMeans.from_json('res/knn_model.txt')
-    # model
-    # pred = model.predict(test)
-    # a = np.zeros((320,), dtype=int)
-    # b = np.ones((80,), dtype=int)
-    # true = np.concatenate([a, b])
-    # # confusion_matrix(true, pred)
-    # return pred
+    # get_session_data(p_id)
+    test = get_session_data(p_id)
+    model = TimeSeriesKMeans.from_json('res/knn_model.txt')
+    pred = model.predict(test)
+
+    leng = len(test)
+    more = int(0.8*leng)
+    less = int(0.2*leng)
+    if more+less < leng:
+        more += 1
+    elif more+less > leng:
+        more -= 1
+    a = np.zeros((more,), dtype=int)
+    b = np.ones((less,), dtype=int)
+    true = np.concatenate([a, b])
+    print(pred)
+    print(confusion_matrix(true, pred))
+    return pred
 
 
 if __name__ == "__main__":
