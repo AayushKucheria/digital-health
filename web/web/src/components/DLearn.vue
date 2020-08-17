971e3388e0299934843875c42e8da8e90f89280a
SophieTruong <sophietruong1992@gmail.com>
2020-08-17 18:45:40 +0300

    add

---
+++ web/web/src/components/DLearn.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+      <h1> Deep Learning Result </h1>
+        <b-table striped hover
+        :items="results">
+        <template v-slot:cell(name)="data">
+          {{ data.value }}
+        </template>
+      </b-table>
+    </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+
+export default {
+  name: 'results',
+  data () {
+    return {
+      result: {
+        patient_id: 0,
+        model_id: 0,
+        result: 0,
+        session_id: 0
+      }
+    }
+  },
+  methods: {
+    ...mapActions([
+      'loadDLResult'
+    ])
+  },
+  computed: {
+    ...mapState([
+      'results'
+    ])
+  },
+  created () {
+    this.loadDLResult(this.$route.params.id)
+  }
+}
+</script>
---
+++ web/web/src/components/KMean.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+      <h1> K-Mean Result</h1>
+        <b-table striped hover
+        :items="results">
+        <template v-slot:cell(name)="data">
+          {{ data.value }}
+        </template>
+      </b-table>
+    </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+
+export default {
+  name: 'results',
+  data () {
+    return {
+      result: {
+        patient_id: 0,
+        model_id: 0,
+        result: 0,
+        session_id: 0
+      }
+    }
+  },
+  methods: {
+    ...mapActions([
+      'loadKMResult'
+    ])
+  },
+  computed: {
+    ...mapState([
+      'results'
+    ])
+  },
+  created () {
+    this.loadKMResult(this.$route.params.id)
+  }
+}
+</script>
