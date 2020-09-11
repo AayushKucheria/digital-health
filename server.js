92b6767ea91dd978d3c6551cf84429fedc16a120
AayushKucheria <aayush.kucheria@gmail.com>
2020-09-12 01:08:33 +0300

    Removed /dist from gitignore and installed express static

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 80
+const port = process.env.PORT || 8080
 app.listen(port);
